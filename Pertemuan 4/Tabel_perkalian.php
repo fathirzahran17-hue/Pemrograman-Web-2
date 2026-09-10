89de8853933b042fcb16f60c3e98df2b3ae35457
fathirzahran17-hue <fathirzahran17@gmail.com>
2026-09-10 15:26:37 +0700

    Tugas

---
+++ Pertemuan 4/Tabel_perkalian.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Tabel Perkalian</title>
+</head>
+
+<body>
+
+<?php
+echo "Tabel Perkalian";
+echo "<br>====================<br>";
+
+for ($i = 15; $i <= 45; $i = $i + 2) {
+    $hasil = 12 * $i;
+    echo "12 * $i = $hasil<br>";
+}
+?>
+
+</body>
+</html>
