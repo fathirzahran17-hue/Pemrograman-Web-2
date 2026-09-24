137cd276f3600a1726410e3041d243a3a229547f
fathirzahran17-hue <fathirzahran17@gmail.com>
2026-09-24 13:18:46 +0700

    Add files via upload

---
+++ Pertemuan 6/Latihan_1.php
@@ -0,0 +1,20 @@
+<?php
+$a[0] = 1;
+$a[1] = 3;
+$a[2] = 5;
+
+$jumlah = count($a);
+
+print "Jumlah array a = $jumlah<br>";
+// Jumlah array a = 3
+
+$b["buah"] = "semangka";
+$b["sayur"] = "wortel";
+$b["daging"] = "ayam";
+$b["utama"] = "nasi";
+
+$jumlah = sizeof($b);
+
+print "Jumlah array b = $jumlah<br>";
+// Jumlah array b = 4
+?>
